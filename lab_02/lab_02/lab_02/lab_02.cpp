1de49119020b075b4474ffd84b2f315a0662e6fe
Bryanskaya <bryanskayakatya@yandex.ru>
2020-10-04 21:16:30 +0300

    ADD: some corrections

--- lab_02/lab_02/lab_02/lab_02.cpp
+++ lab_02/lab_02/lab_02/lab_02.cpp
@@ -92,10 +92,8 @@ int main()
         return -1;
     }
 
-    //vector<int> n1{ 50, 100, 200, 300, 400, 500 };
-    //vector<int> n2{ 51, 101, 201, 301, 401, 501 };
-    vector<int> n1{600, 700};
-    vector<int> n2{601, 701};
+    vector<int> n1{ 50, 100, 200, 300, 400, 500, 600, 700 };
+    vector<int> n2{ 51, 101, 201, 301, 401, 501, 601, 701 };
     test_range(n1);
     test_range(n2);
 
