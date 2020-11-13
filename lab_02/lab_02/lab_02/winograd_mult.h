a611260ee36631822556398d7d4f77a4e2bd1233
Bryanskaya <bryanskayakatya@yandex.ru>
2020-11-13 13:36:11 +0300

    TRY: to delete useless files

--- lab_02/lab_02/lab_02/winograd_mult.h
+++
@@ -1,9 +0,0 @@
-#ifndef WINOGRAD_MULT_H
-#define WINOGRAD_MULT_H
-
-#include "matrix.h"
-
-matrix_t winograd_mult(matrix_t a, matrix_t b, int m, int n, int q);
-matrix_t winograd_opt_mult(matrix_t a, matrix_t b, int m, int n, int q);
-
-#endif // WINOGRAD_MULT_H
